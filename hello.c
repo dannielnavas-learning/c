e022961da0ae093616479dbf919c5288187c0cd2
Danniel Navas <dannielnavas@gmail.com>
2026-08-31 07:30:26 -0500

    Agregar comentarios explicativos sobre tipos básicos en hello.c

--- hello.c
+++ hello.c
@@ -8,3 +8,19 @@ int main()
     printf("Hello, World!\n"); // printf() displays the string inside quotation
     return 0;                  // return statement close the main function
 }
+
+// Basic types:
+// arithmetic types: (valores enteros)
+// int 4 Bytes -2,147,483,648 to 2,147,483,647
+// no negativos unsigned int de 0 a 4,294,967,295 (4 bytes)
+
+// short 2 Bytes  -32768 to 32767
+// unsigned short 0 to 65535 (2 bytes)
+
+// long 8 Bytes -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+// unsigned long 0 to 18,446,744,073,709,551,615 (8 bytes)
+
+//  De punto flotante:
+// float 4 Bytes 1.2E-38 to 3.4E+38 6 decimal places
+// double 8 Bytes 2.3E-308 to 1.7E+308 15 decimal places
+// long double 10 Bytes 3.4E-4932 to 1.1E+4932 19 decimal places
