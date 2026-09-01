04f03530a7113bd0b023ff18af1fb5d34f4358a5
Danniel Navas <dannielnavas@gmail.com>
2026-09-01 07:39:48 -0500

    Agregar archivo variables.c con ejemplos de declaraciones y operaciones básicas de variables

---
+++ variables.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// int i, j, k;
+// int parking;             // permite tanto positivo como negativo
+// int minutesParked = 10;  // con inicialización
+// unsigned int totalCard;  // solo permite valores positivos
+// char initial = 'A';      // variable de tipo caracter con inicialización
+// char dailyWorkHours = 0; // variable de tipo caracter con inicialización
+
+// int main()
+// {
+//     return 0;
+// }
+
+// Variable declarations
+// extern int  cuando se usan varios archivos
+
+int a, b, c;
+
+float f, g, h;
+
+int main()
+{
+    // Assign values to integer variables
+    a = 5;
+    b = 10;
+    c = a + b;
+    printf("a = %d, b = %d, c = %d\n", a, b, c);
+    printf("La suma es: %d\n", c); // el %d se usa para imprimir enteros es la posicion donde se coloca la variable correspondiente
+
+    f = 1.1;
+    g = 2.2;
+    h = f + g;
+    printf("f = %.1f, g = %.1f, h = %.1f\n", f, g, h); // el %.1f se usa para imprimir flotantes
+    printf("La suma de flotantes es: %.1f\n", h);      // el %.1f se usa para imprimir flotantes
+    return 0;
+}
