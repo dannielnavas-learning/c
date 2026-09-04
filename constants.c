a47c9016075209d02b018c1c9a2ef92e264b1cc6
Danniel Navas <dannielnavas@gmail.com>
2026-09-04 07:47:04 -0500

    Agregar archivo constants.c con definiciones de constantes y comentarios explicativos

---
+++ constants.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// "este es un mensaje" en c no existe el string lo que hace es dejarlo en un array de caracteres cada letra y espacio y termina con ul null
+
+const int costHotdog = 5u; // se puede usar 100u unsigned o l long tambien sie puede guardar palabras como "hola"
+const float iva = 0.16;    // seis espacios despues del punto por el tipo float
+const char moneda = '$';   // se puede dejar a un char un salto de linea  con la convecion backslash (\) + n
+
+int main()
+{
+    return 0;
+}
