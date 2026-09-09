baf4ee8b21360a1189a3526342c5e803dc1880b0
Danniel Navas <dannielnavas@gmail.com>
2026-09-09 07:10:18 -0500

    Agregar archivo for.c con implementación de bucle for para iterar un array conocido

---
+++ for.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 5; i++) // aplica cuando conocemos el tamaño del bucle de antemano se utiliza for para iterar arrays
+    {
+        printf("i = %d\n", i);
+    }
+    return 0;
+}
