10337efb585cb6b11fe117599f1bd3e271a630b5
Danniel Navas <dannielnavas@gmail.com>
2026-09-10 07:40:10 -0500

    Agregar archivo functionTypesPerReturnType.c con ejemplos de funciones en C

---
+++ functionTypesPerReturnType.c
@@ -0,0 +1,42 @@
+// funciones que sin argunmentos y sin valor de retorno.
+
+// void functionName(); le dice al compilador que la funcion existe.
+
+// functionName(); llamado a la funcion
+
+// void functionName() { esta seria la definicion de la funcion
+
+// }
+
+// Funciones con argumentos pero sin valores a retornar
+
+// void functionName(int arg1, int arg2);
+
+// functionName(int arg1, int arg2);
+
+// void functionName(int arg1, int arg2) // puede ser de cualquier tipo de dato
+// {
+// cuerpo de la funcion
+// }
+
+// C se cataloga como un lenguaje débilmente tipado pero el compilador si solicita el tipo
+
+// Funcion sin argumentos pero que si retorna un valor
+
+// int functionName();
+
+// functionName(); llamado a la funcion que retorna un valor
+
+// int functionName() {
+//     return 0;
+// }
+
+// Funciones con argumentos y que tienen return value
+
+// float functionName(float arg1, float arg2);
+
+// functionName(float arg1, float arg2);
+
+// float functionName(float arg1, float arg2) {
+//     return 0.0;
+// }
